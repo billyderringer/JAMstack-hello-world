bf4e89e8c1e8339002e46713bacdee1e00ea49df
Billy Derringer <billyderringer7@gmail.com>
2018-08-27 18:39:47 -0500

    fixed page 1 punctuation

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi I'm Billy Derringer</h1>
+    <h1>Hi, I'm Billy Derringer</h1>
     <p>I am a front-end developer from Dallas, TX</p>
     <p>Let's go build something great!</p>
     <Link to="/page-2/">Connect With Me</Link>
