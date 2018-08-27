581868b45470682abc8790da60374c3f15a4a3fb
Billy Derringer <billyderringer7@gmail.com>
2018-08-27 17:59:41 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi I'm Billy Derringer</h1>
+    <p>I am a front-end developer from Dallas, TX</p>
+    <p>Let's go build something great!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
